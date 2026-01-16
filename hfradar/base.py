cbbe7d56d94f05c673c9d7b4e52039a9904aa3c8
Jordi Isern-Fontanet <jisern@icm.csic.es>
2026-01-16 17:17:42 +0100

    Added the lmercator() function

--- hfradar/base.py
+++ hfradar/base.py
@@ -7,6 +7,50 @@ import os
 import numpy as np
 
 
+def lmercator(xin_, yin_, lon0=0, lat0=0, radius=6371000.0, inverse=False):
+    """
+    Compute the Mercator projection centered at the center of the image
+
+    Parameters
+    -----
+    xin: ndarray
+        Longitude (inverse=False) or x-coordinate (inverse=True)
+    yin: ndarray
+        Latitude (inverse=False) or y-coordinate (inverse=True)
+    lon0: float, optional, default=0
+        Central longitude used to compute the projection (inverse=True)
+    lat0: float, optional, default=0
+        Central latitude used to compute the projection (inverse=True)
+
+    Returns
+    -------
+    xout: ndarray
+        x-coordinate (inverse=False) or Longitude (inverse=True)
+    yout: ndarray
+        y-coordinate (inverse=False) or Latitude (inverse=True)
+    """
+
+    dtor = np.pi / 180
+    rtod = 1 / dtor
+
+    if inverse:
+        x = xin_.copy()
+        y = yin_.copy()
+        lon = x / radius
+        lat = 2 * np.arctan(np.exp(y / radius)) - np.pi / 2.
+        xout = lon * rtod + lon0
+        yout = lat * rtod + lat0
+    else:
+        lon = (xin_ - lon0) * dtor
+        lat = (yin_ - lat0) * dtor
+        x = radius * lon
+        y = radius * np.log(np.tan(np.pi / 4 + lat / 2.))
+        xout = x
+        yout = y
+
+    return xout, yout
+
+
 def get_hfr_noise(datain, maxit=10, ksigma=3, return_coeff=False, nobs=None):
 
     # B3-Splines
